58aaeb809a8542455562c1eaad2f0d134fbc7f6c
Eng. Sarah Seenne <sb12321b@gmail.com>
2025-08-31 21:22:29 +0300

    added a personla phone number

--- src/comp/navbar/Navbar.jsx
+++ src/comp/navbar/Navbar.jsx
@@ -4,17 +4,18 @@ import { useState, useEffect } from "react";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [chooseMenu, setChooseMenu] = useState('Home');
+  const [chooseMenu, setChooseMenu] = useState("Home");
   const locate = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
   };
 
-  useEffect(()=>{
-    switch (locate.pathname){
-      case '/reval-world-sample2/': setChooseMenu("Home");
-      break;
+  useEffect(() => {
+    switch (locate.pathname) {
+      case "/reval-world-sample2/":
+        setChooseMenu("Home");
+        break;
       case "/reval-world-sample2/services":
         setChooseMenu("Services");
         break;
@@ -27,17 +28,16 @@ export const Navbar = () => {
       case "/reval-world-sample2/contact":
         setChooseMenu("Contact");
         break;
-        case "/reval-world-sample2/colors":
+      case "/reval-world-sample2/colors":
         setChooseMenu("Colors");
         break;
-        case "/reval-world-sample2/industrial":
+      case "/reval-world-sample2/industrial":
         setChooseMenu("Industrial");
         break;
       default:
         setChooseMenu("Home");
     }
   }, [locate]);
-
 
   return (
     <nav>
@@ -46,18 +46,69 @@ export const Navbar = () => {
       </h1>
       <div className="info">
         <ul className={`dropdown ${menuOpen ? "show" : ""}`}>
-          <li className={chooseMenu === "Home" ? "clicked" : ""}><Link className="a" to="/reval-world-sample2/">Home</Link></li>
-          <li className={chooseMenu === "Services" ? "clicked" : ""}><a href="#ser" className="a">Services</a></li>
-          <li className={chooseMenu === "Projects" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/projects'> Projects</Link></li>
-          <li className={chooseMenu === "About Us" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/about'> About Us</Link></li>
-          <li className={chooseMenu === "Contact" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/contact'>Contact</Link> </li>
-          <li className={chooseMenu === "Colors" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/colors'>Colors</Link> </li>
-          <li className={chooseMenu === "Colors" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/colors'>UPVC</Link> </li>
-          <li className={chooseMenu === "Industrail" ? "clicked" : ""}> <Link className="a" to='/reval-world-sample2/Industrial'>Industrial</Link> </li>
+          <li className={chooseMenu === "Home" ? "clicked" : ""}>
+            <Link className="a" to="/reval-world-sample2/">
+              Home
+            </Link>
+          </li>
+          <li className={chooseMenu === "Services" ? "clicked" : ""}>
+            <a href="#ser" className="a">
+              Services
+            </a>
+          </li>
+          <li className={chooseMenu === "Projects" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/projects">
+              {" "}
+              Projects
+            </Link>
+          </li>
+          <li className={chooseMenu === "About Us" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/about">
+              {" "}
+              About Us
+            </Link>
+          </li>
+          <li className={chooseMenu === "Contact" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/contact">
+              Contact
+            </Link>{" "}
+          </li>
+          <li className={chooseMenu === "Colors" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/colors">
+              Colors
+            </Link>{" "}
+          </li>
+          <li className={chooseMenu === "Colors" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/colors">
+              UPVC
+            </Link>{" "}
+          </li>
+          <li className={chooseMenu === "Industrail" ? "clicked" : ""}>
+            {" "}
+            <Link className="a" to="/reval-world-sample2/Industrial">
+              Industrial
+            </Link>{" "}
+          </li>
         </ul>
         <ul className="contact-info">
-          <li><i className="fa-solid fa-phone"></i>+966 123456789</li>
-          <li><i className="fa-brands fa-whatsapp"></i>Whatsapp</li>
+          <li>
+            <i className="fa-solid fa-phone"></i>+966 123456789
+          </li>
+          <li>
+            <a
+              className="a"
+              href="https://wa.me/966583319303?text=Hello%20there!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-whatsapp"></i>Whatsapp
+            </a>
+          </li>
           <i
             className={`fa-solid fa-2xl ${menuOpen ? "fa-xmark" : "fa-bars"}`}
             id="menuBar"
