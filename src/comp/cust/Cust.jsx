5389afb95ef42e953634e2f26106a7fb4f045350
Eng. Sarah Seenne <sb12321b@gmail.com>
2025-08-31 19:29:00 +0300

    respo and cust

--- src/comp/cust/Cust.jsx
+++ src/comp/cust/Cust.jsx
@@ -4,14 +4,16 @@ import data from "../../data/targetedCustomers";
 export const Cust = () => {
   return (
     <div className="cust">
-        <h1>Our Targeted Customers</h1>
+      <h1>Our Targeted Customers</h1>
       <div className="cont">
         {data.map((item) => {
           return (
             <div className="cust-item">
               <i className={item.iconClassName}></i>
-              <h5>{item.enName}</h5>
-              <h5 className="ar">{item.arName}</h5>
+              <div className="txt">
+                <h5>{item.enName}</h5>
+                <h5 className="ar">{item.arName}</h5>
+              </div>
             </div>
           );
         })}
